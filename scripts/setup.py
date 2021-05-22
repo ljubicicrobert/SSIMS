231d56b0368ceebca0c1b20630931f94dffa01b5
ljubicicrobert <71554915+ljubicicrobert@users.noreply.github.com>
2021-05-22 15:02:43 +0200

    Add files via upload

---
+++ scripts/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+setuptools.setup(name='SSIMS: SSIM-based video stabilization suite"',
+				 version='0.1.3',
+				 author='Robert Ljubicic',
+				 author_email='rljubicic@grf.bg.ac.rs',
+				 url='https://github.com/ljubicicrobert/SSIMS',
+				 description='Video stabilization suite for UAS/UAV purposes based on Structural Similarity Index metric',
+				 keywords=['Unmanned Aerial Systems', 'UAS', 'Unmanned Aerial Vehicles', 'UAV', 'stabilization', 'velocimetry'],
+				 maintainer='Robert Ljubicic, University of Belgrade - Civil Engineering Faculty',
+				 maintainer_email='rljubicic@grf.bg.ac.rs',
+				 classifiers=[
+				     'Development Status :: 4 - Beta',
+				     'Environment :: Console (Text Based)',
+				     'Programming Language :: Python :: 3',
+			     ],
+				 install_requires=[
+					 'numpy>=1.1',
+					 'opencv_python>=4',
+					 'matplotlib>=3',
+					 'scipy>=1',
+					 'skimage>=0.16.1',
+				 ],
+				 python_requires='>=3.5',
+				 license='GPL 3.0',
+				 packages=setuptools.find_packages(),
+				 scripts=[
+					 '__init__.py',
+					 'feature_tracking.py',
+					 'stabilize_frames.py',
+					 'feature_goodness.py',
+					 'unpack_video.py',
+					 'frames_to_video.py',
+				 ],
+)
